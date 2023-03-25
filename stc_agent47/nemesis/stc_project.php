f21f109dbb2da468c47809b2ff9311f7567d6b3f
DESKTOP-171LBS0\N2 <nausher.khan12@gmail.com>
2023-03-25 22:23:24 +0530

    stc agent manager response change for status down list to manager and top of manager

--- stc_agent47/nemesis/stc_project.php
+++ stc_agent47/nemesis/stc_project.php
@@ -1776,7 +1776,7 @@ class pirates_supervisor extends tesseract{
 
 				$actionsec='';
 				if($_SESSION['stc_agent_role']==3){
-					if($row['stc_status_down_list_status']==4){
+					if(($row['stc_status_down_list_status']==4) || ($row['stc_status_down_list_status']==3)){
 						$actionsec='
 							<a href="#" class="stc-set-to-close" style="font-size:20px" id="'.$row['stc_status_down_list_id'].'"><i class="fas fa-thumbs-up"></i></a>
 						';
