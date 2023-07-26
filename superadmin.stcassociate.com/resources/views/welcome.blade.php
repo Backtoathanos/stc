266ffc64201d103049eb3d886ef01318d3a72c26
DESKTOP-171LBS0\N2 <nausher.khan12@gmail.com>
2023-07-26 17:50:35 +0530

    ass

--- superadmin.stcassociate.com/resources/views/welcome.blade.php
+++ superadmin.stcassociate.com/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    STC ASSociates Super Admin
+                    STC Associates Super Admin
                 </div>
             </div>
         </div>
