0360d63b3b4dadf5bee17ad4706525412227af26
“Nausher <nausher.khan12@gmail.com>
2023-11-21 22:31:33 -0700

    yeah

--- stc_sub_agent47/nemesis/stc_std.php
+++ stc_sub_agent47/nemesis/stc_std.php
@@ -479,7 +479,7 @@ class transformers extends tesseract{
 				}
 
 				$actionsec='';
-				if($_SESSION['stc_agent_sub_category']=="Supervisor"){
+				if($_SESSION['stc_agent_sub_category']=="Supervisor" || $_SESSION['stc_agent_sub_category']=="Site Incharge"){
 					if($row['stc_status_down_list_status']==1){
 						$actionsec='
 							<a href="javascript:void(0)" class="btn bg-danger text-white mb-3 stc-std-operation-btn" type="update" data-toggle="modal" data-target=".bd-create-std-modal" id="'.$row['stc_status_down_list_id'].'"><i class="pe-7s-pen"></i></a>
@@ -512,7 +512,8 @@ class transformers extends tesseract{
 					}else{
 						$actionsec='#';
 					}
-				}elseif($_SESSION['stc_agent_sub_category']=="Site Incharge"){
+				}
+				if($_SESSION['stc_agent_sub_category']=="Site Incharge"){
 					if($row['stc_status_down_list_status']==4){
 						$actionsec='
 								<div class="col-sm-12 col-md-12">
