591bd103881536e0d9cc776a5adc7c5a04e9a6bd
Nausher Khan - i712700k <nausher.khan12@gmail.com>
2024-06-21 18:25:11 +0530

    latest changes

--- stc_sub_agent47/sidebar-nav.php
+++ stc_sub_agent47/sidebar-nav.php
@@ -105,6 +105,12 @@
                                     Status Down List
                                 </a>
                             </li>
+                            <li>
+                                <a href="epermitenroll.php?page=epermitenroll" class="epermitenroll">
+                                    <i class="metismenu-icon pe-7s-add-user"></i>
+                                    E-Permit Enrollment
+                                </a>
+                            </li>
                         ';
                     }
                     if($_SESSION['stc_agent_sub_category']=="Safety Supervisor"){
