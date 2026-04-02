8d146a75689f391c6fd023d3b05481f611724ee5
Nausher Khan <nausher.khan12@gmail.com>
2026-04-02 13:29:15 +0530

    latest changes

--- MCU/db.php
+++ MCU/db.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Asia/Kolkata');
 // Docker / EC2: set DB_HOST=mysql, DB_USER, DB_PASSWORD, DB_NAME via environment (see docker-compose.yml)
-$server = getenv('DB_HOST') ?: 'mysql-service';
+$server = getenv('DB_HOST') ?: 'localhost';
 $user = getenv('DB_USER') ?: 'nausher';
 $password = getenv('DB_PASSWORD') ?: 'ZX5#*,PPq$5;';
 $db = getenv('DB_NAME') ?: 'stc_associate_go';
