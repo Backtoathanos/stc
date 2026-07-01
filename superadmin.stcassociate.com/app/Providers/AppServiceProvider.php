bcf2943e70884cc6617fee2fc0c94d3f69215fef
Nausher Khan <nausher.khan12@gmail.com>
2026-07-01 16:31:41 +0530

    laravel issue

--- superadmin.stcassociate.com/app/Providers/AppServiceProvider.php
+++ superadmin.stcassociate.com/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if ($this->app->runningInConsole()) {
+            return;
+        }
+
+        $request = request();
+        $useHttps = $request->isSecure()
+            || $request->header('X-Forwarded-Proto') === 'https'
+            || filter_var(env('FORCE_HTTPS', false), FILTER_VALIDATE_BOOLEAN);
+
+        if ($useHttps) {
+            URL::forceScheme('https');
+        }
     }
 }
 
